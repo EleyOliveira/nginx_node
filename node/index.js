cde560d8f9b8d615609d3976f783496d91a2d04d
EleyOliveira <eleyoliveira@bol.com.br>
2023-11-16 18:51:11 -0300

    correção arquivo Dockerfile config nginx

--- node/index.js
+++ node/index.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs')
 app.set('views', './views')
 
 app.get('/', async(req, res) => {   
-    await createPeople('Lulu Silva')         
+    await createPeople('Lili Peres')         
     const people = await getPeople()
     res.render('home', {people: people})        
 });
